d6a6a6a2fad98ff79f1497e1a3e7799f3308a6a9
hyoongshin_acksal <vaishaalinc@gmail.com>
2025-08-13 19:31:10 +0530

    solve PermutationOfString II

--- recursion/level02/PermutationOfString.java
+++ recursion/level02/PermutationOfString.java
@@ -1,5 +1,6 @@
 package recursion.level02;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class PermutationOfString {
@@ -7,10 +8,11 @@ public class PermutationOfString {
 
   public static void main(String[] args) {
     count = 0;
-    var s = "abbaa";
+    var s = "abba";
     var map = new HashMap<Character, Integer>();
-    for (var c : s.toCharArray()) map.merge(c, 1, Integer::sum);
+    for (var c : s.toCharArray()) map.put(c, -1);
     solve(1, s.length(), map, "");
+    solve(0, s.length(), map, new Character[s.length()], s);
   }
 
   private static void solve(int cp, int tp, HashMap<Character, Integer> map, String asf) {
@@ -24,6 +26,26 @@ public class PermutationOfString {
         map.merge(ch, -1, Integer::sum);
         solve(cp + 1, tp, map, asf + ch);
         map.merge(ch, 1, Integer::sum);
+      }
+    }
+  }
+
+  private static void solve(
+      int cc, int tc, HashMap<Character, Integer> map, Character[] arr, String s) {
+    if (cc == tc) {
+      System.out.println(++count + " " + Arrays.toString(arr));
+      return;
+    }
+
+    var ch = s.charAt(cc);
+    var lo = map.get(ch);
+    for (int i = lo + 1; i < arr.length; i++) {
+      if (arr[i] == null) {
+        arr[i] = ch;
+        map.put(ch, i);
+        solve(cc + 1, tc, map, arr, s);
+        arr[i] = null;
+        map.put(ch, lo);
       }
     }
   }
