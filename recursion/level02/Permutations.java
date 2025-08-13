37e82106e0d78e5df80c66b6ac652e0aa8873c26
hyoongshin_acksal <vaishaalinc@gmail.com>
2025-08-13 18:46:16 +0530

    solve Permutations II

--- recursion/level02/Permutations.java
+++ recursion/level02/Permutations.java
@@ -3,19 +3,23 @@ package recursion.level02;
 import java.util.Arrays;
 
 public class Permutations {
-    private static int count;
+  private static int count;
+
   public static void main(String[] args) {
-      count=0;
+    count = 0;
     var box = 4;
     var obj = 2;
 
     var arr = new int[box];
     solve(arr, 1, obj);
+    count = 0;
+    var used = new boolean[obj];
+    solve(used, 0, 1, box, "");
   }
 
   private static void solve(int[] arr, int c, int t) {
     if (c > t) {
-      System.out.println(++count+" "+Arrays.toString(arr));
+      System.out.println(++count + " " + Arrays.toString(arr));
       return;
     }
     for (int i = 0; i < arr.length; i++) {
@@ -25,5 +29,20 @@ public class Permutations {
         arr[i] = 0;
       }
     }
+  }
+
+  private static void solve(boolean[] used, int tot, int cur, int boxes, String asf) {
+    if (cur > boxes) {
+      if (tot == used.length) System.out.println(++count + " " + asf);
+      return;
+    }
+    for (int i = 0; i < used.length; i++) {
+      if (!used[i]) {
+        used[i] = true;
+        solve(used, tot + 1, cur + 1, boxes, asf + (i + 1));
+        used[i] = false;
+      }
+    }
+    solve(used, tot, cur + 1, boxes, asf + '0');
   }
 }
